706c695ec5133458b6a84a150bdb223515861837
eod940 <eod940@gmail.com>
2023-10-24 01:04:16 +0900

    init: project start

---
+++ src/main/java/com/valuewith/tweaver/TweaverApplication.java
@@ -0,0 +1,13 @@
+package com.valuewith.tweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valuewith/tweaver/TweaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valuewith.tweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
