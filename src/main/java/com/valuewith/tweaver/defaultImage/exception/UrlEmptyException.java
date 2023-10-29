0876a52809162d6fcac4e3ac48bb94aeec153449
wooooozin <wooooozin@gmail.com>
2023-10-29 17:23:08 +0900

    feat: 커스텀 클래스 생성 및 적용

---
+++ src/main/java/com/valuewith/tweaver/defaultImage/exception/UrlEmptyException.java
@@ -0,0 +1,20 @@
+package com.valuewith.tweaver.defaultImage.exception;
+
+
+import com.valuewith.tweaver.constants.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class UrlEmptyException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public UrlEmptyException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getDescription();
+        this.httpStatus = errorCode.getHttpStatus();
+    }
+}
