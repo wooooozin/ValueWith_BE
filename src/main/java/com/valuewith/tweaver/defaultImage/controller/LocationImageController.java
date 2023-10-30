760236225f712e98cf030713602471ca956bb1c2
wooooozin <wooooozin@gmail.com>
2023-10-30 21:33:05 +0900

    feat: SwaggerAPI 구현, 멤버 디폴트 이미지 컨트롤러 구현

--- src/main/java/com/valuewith/tweaver/defaultImage/controller/LocationImageController.java
+++ src/main/java/com/valuewith/tweaver/defaultImage/controller/LocationImageController.java
@@ -3,6 +3,7 @@ package com.valuewith.tweaver.defaultImage.controller;
 import com.valuewith.tweaver.constants.ImageType;
 import com.valuewith.tweaver.defaultImage.dto.DefaultImageResponseDto;
 import com.valuewith.tweaver.defaultImage.service.ImageService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ public class LocationImageController {
 
     private final ImageService imageService;
 
+    @ApiOperation(value = "로케이션 이미지 업로드", notes = "S3 이미지 업로드 테스트 POST API")
     @PostMapping("/test-upload")
     public ResponseEntity<String> uploadLocationImage(
         @RequestParam("file") MultipartFile file
@@ -28,6 +30,7 @@ public class LocationImageController {
         return ResponseEntity.ok(url);
     }
 
+    @ApiOperation(value = "로케이션 이미지 수정", notes = "S3 이미지 수정하는 테스트 POST API")
     @PostMapping("/test-update")
     public ResponseEntity<String> modifyLocationImage(
         @RequestParam("file") MultipartFile newFile,
@@ -37,13 +40,32 @@ public class LocationImageController {
         return ResponseEntity.ok(url);
     }
 
-    @PostMapping("/upload")
+    @ApiOperation(
+        value = "로케이션 디폴트 이미지 업로드 API",
+        notes = "디폴트 지역 이미지 추가 api 입니다. name에는 지역명을 입력해주세요"
+    )
+    @PostMapping("/upload-location")
     public ResponseEntity<DefaultImageResponseDto> createLocationImage(
         @RequestParam("file") MultipartFile file,
         @RequestParam("name") String locationName
     ) {
         String imageUrl = imageService.randomLocationImageUploadAndGetUrl(file, ImageType.LOCATION, locationName);
         DefaultImageResponseDto responseDto = DefaultImageResponseDto.from(locationName, imageUrl);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @ApiOperation(
+        value = "멤버 디폴트 이미지 업로드 API",
+        notes = "디폴트 멤버 이미지 추가 api 입니다. name에는 '멤버' 를 입력해주세요. "
+            + "프로필 이미지 미 설정시 defaultImage repository에서 '멤버'로 find해서 url 넘겨주면 됩니다."
+    )
+    @PostMapping("/upload-member")
+    public ResponseEntity<DefaultImageResponseDto> createMemberImage(
+        @RequestParam("file") MultipartFile file,
+        @RequestParam("name") String memberName
+    ) {
+        String imageUrl = imageService.randomLocationImageUploadAndGetUrl(file, ImageType.MEMBER, memberName);
+        DefaultImageResponseDto responseDto = DefaultImageResponseDto.from(memberName, imageUrl);
         return ResponseEntity.ok(responseDto);
     }
 }
