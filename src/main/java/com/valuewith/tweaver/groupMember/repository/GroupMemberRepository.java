ff32683fb1a9e98441695b8eb5a4608f6655284b
wooooozin <wooooozin@gmail.com>
2023-11-04 03:16:06 +0900

    refactor: @ Formula 적용으로 메서드 삭제

--- src/main/java/com/valuewith/tweaver/groupMember/repository/GroupMemberRepository.java
+++ src/main/java/com/valuewith/tweaver/groupMember/repository/GroupMemberRepository.java
@@ -1,12 +1,9 @@
 package com.valuewith.tweaver.groupMember.repository;
 
-import com.valuewith.tweaver.constants.ApprovedStatus;
-import com.valuewith.tweaver.group.entity.TripGroup;
 import com.valuewith.tweaver.groupMember.entity.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
   void deleteGroupMemberByTripGroupTripGroupId(Long tripGroupId);
 
-  Integer countApprovedMembers(TripGroup tripGroup, ApprovedStatus approvedStatus);
 }
