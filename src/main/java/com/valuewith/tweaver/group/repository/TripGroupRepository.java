eebaf5f93f9c8177cdd23115ea59945bdee00d61
wooooozin <wooooozin@gmail.com>
2023-11-04 00:25:28 +0900

    refactor: 동적쿼리 적용을 위한 QueryDSL 재설정

--- src/main/java/com/valuewith/tweaver/group/repository/TripGroupRepository.java
+++ src/main/java/com/valuewith/tweaver/group/repository/TripGroupRepository.java
@@ -1,16 +1,10 @@
 package com.valuewith.tweaver.group.repository;
 
-import com.valuewith.tweaver.constants.GroupStatus;
 import com.valuewith.tweaver.group.entity.TripGroup;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TripGroupRepository extends JpaRepository<TripGroup, Long> {
-    Page<TripGroup> findByStatusAndTripAreaAndNameContainingIgnoreCase(
-        GroupStatus status, String area, String name, Pageable pageable
-    );
 
 }
--- src/main/java/com/valuewith/tweaver/group/service/TripGroupListService.java
+++ src/main/java/com/valuewith/tweaver/group/service/TripGroupListService.java
@@ -1,18 +1,26 @@
 package com.valuewith.tweaver.group.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.valuewith.tweaver.constants.ApprovedStatus;
 import com.valuewith.tweaver.constants.GroupStatus;
 import com.valuewith.tweaver.group.dto.TripGroupListResponseDto;
 import com.valuewith.tweaver.group.dto.TripGroupResponseDto;
+import com.valuewith.tweaver.group.entity.QTripGroup;
 import com.valuewith.tweaver.group.entity.TripGroup;
 import com.valuewith.tweaver.group.repository.TripGroupRepository;
+import com.valuewith.tweaver.groupMember.entity.QGroupMember;
 import com.valuewith.tweaver.groupMember.repository.GroupMemberRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -20,29 +28,77 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TripGroupListService {
 
-    private final TripGroupRepository tripGroupRepository;
     private final GroupMemberRepository groupMemberRepository;
+
+    private static final QTripGroup qTripGroup = QTripGroup.tripGroup;
+    private static final QGroupMember qGroupMember = QGroupMember.groupMember;
+    private final JPAQueryFactory queryFactory;
 
     public TripGroupListResponseDto getFilteredTripGroups(
         String status, String area, String title, Pageable pageable
     ) {
-        Page<TripGroup> page = tripGroupRepository.findByStatusAndTripAreaAndNameContainingIgnoreCase(
-            GroupStatus.valueOf(status.toUpperCase()), area, title, pageable
-        );
-        List<TripGroupResponseDto> tripGroupResponseDtoList = page.getContent().stream()
+        /*
+        BooleanBuilder는 QueryDSL의 조건을 구성하는 데 사용되며,
+        여러 필터링 조건을 동적으로 결합할 수 있음.
+         */
+        BooleanBuilder predicate = new BooleanBuilder();
+        if (!"all".equalsIgnoreCase(status)) {
+            predicate.and(qTripGroup.status.eq(GroupStatus.valueOf(status.toUpperCase())));
+        }
+        if (!"all".equalsIgnoreCase(area)) {
+            predicate.and(qTripGroup.tripArea.eq(area));
+        }
+        if (title != null && !title.trim().isEmpty()) {
+            predicate.and(qTripGroup.name.containsIgnoreCase(title.trim()));
+        }
+
+        List<TripGroup> tripGroups = queryFactory
+            .selectFrom(qTripGroup)
+            .where(predicate)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(sortOrder(pageable.getSort()).stream().toArray(OrderSpecifier[]::new))
+            .fetch();
+
+        List<TripGroupResponseDto> tripGroupResponseDtoList = tripGroups.stream()
             .map(tripGroup -> {
-                int currentMembersCount = groupMemberRepository.countApprovedMembers(tripGroup, ApprovedStatus.APPROVED) + 1;
+                int currentMembersCount =
+                    groupMemberRepository.countApprovedMembers(tripGroup, ApprovedStatus.APPROVED)
+                        + 1;
                 return TripGroupResponseDto.from(tripGroup, currentMembersCount);
             })
             .collect(Collectors.toList());
 
+        long total = queryFactory
+            .select(qTripGroup.count())
+            .from(qTripGroup)
+            .where(predicate)
+            .fetchOne();
+
+        int totalPages = (int) Math.ceil((double) total / pageable.getPageSize());
+        boolean isLast = pageable.getOffset() + pageable.getPageSize() >= total;
         return TripGroupListResponseDto.from(
             tripGroupResponseDtoList,
-            page.getNumber(),
-            page.getTotalPages(),
-            page.getTotalElements(),
-            page.isLast()
+            pageable.getPageNumber(),
+            totalPages,
+            total,
+            isLast
         );
     }
+
+    // 스프링의 Sort 객체를 QueryDSL OrderSpecifier로 변환
+    private List<OrderSpecifier> sortOrder(Sort sort) {
+        List<OrderSpecifier> orders = new ArrayList<>();
+        //sort
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+            PathBuilder orderByExpression = new PathBuilder<>(TripGroup.class, "tripGroup");
+            orders.add(new OrderSpecifier(direction, orderByExpression.get(prop)));
+        });
+        return orders;
+    }
+
+}
 }
 
