08962e03fa3a4e99d8997bcc8a10721566cc8eb3
wooooozin <wooooozin@gmail.com>
2023-11-04 17:13:25 +0900

    feat: PlaceRepository 에 PlaceRepositoryCustom 적용

--- src/main/java/com/valuewith/tweaver/place/repository/PlaceRepository.java
+++ src/main/java/com/valuewith/tweaver/place/repository/PlaceRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlaceRepository extends JpaRepository<Place, Long> {
+public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
   List<Place> deletePlacesByTripGroup(TripGroup tripGroup);
 
   void deleteAllByTripGroupTripGroupId(Long tripGroupId);
+
+
+  List<Place> findByTripGroup(TripGroup tripGroup);
 }
