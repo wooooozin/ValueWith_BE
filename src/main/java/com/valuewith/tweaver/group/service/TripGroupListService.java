d92c7151afd6be56834e88d21d4fe58e28ad9032
wooooozin <wooooozin@gmail.com>
2023-11-04 17:17:47 +0900

    feat: getTripGroupDetail 서비스 메서드 구현 및 기존 메서드 기능 분리
    기존 getFilteredTripGroups 서비스 메서드 customRepository 구현 및 적용으로 기능 분리

--- src/main/java/com/valuewith/tweaver/group/service/TripGroupListService.java
+++ src/main/java/com/valuewith/tweaver/group/service/TripGroupListService.java
@@ -1,76 +1,42 @@
 package com.valuewith.tweaver.group.service;
 
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Order;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.PathBuilder;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.valuewith.tweaver.constants.GroupStatus;
+import com.valuewith.tweaver.constants.ApprovedStatus;
+import com.valuewith.tweaver.group.dto.TripGroupDetailResponseDto;
 import com.valuewith.tweaver.group.dto.TripGroupListResponseDto;
 import com.valuewith.tweaver.group.dto.TripGroupResponseDto;
-import com.valuewith.tweaver.group.entity.QTripGroup;
 import com.valuewith.tweaver.group.entity.TripGroup;
-import com.valuewith.tweaver.groupMember.entity.QGroupMember;
+import com.valuewith.tweaver.group.repository.TripGroupRepository;
+import com.valuewith.tweaver.groupMember.dto.GroupMemberDetailResponseDto;
 import com.valuewith.tweaver.groupMember.repository.GroupMemberRepository;
-import java.util.ArrayList;
+import com.valuewith.tweaver.place.dto.PlaceDetailResponseDto;
+import com.valuewith.tweaver.place.repository.PlaceRepository;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TripGroupListService {
 
+    private final TripGroupRepository tripGroupRepository;
     private final GroupMemberRepository groupMemberRepository;
+    private final PlaceRepository placeRepository;
 
-    private static final QTripGroup qTripGroup = QTripGroup.tripGroup;
-    private static final QGroupMember qGroupMember = QGroupMember.groupMember;
-    private final JPAQueryFactory queryFactory;
-
-    @Transactional
     public TripGroupListResponseDto getFilteredTripGroups(
         String status, String area, String title, Pageable pageable
     ) {
-        /*
-        BooleanBuilder는 QueryDSL의 조건을 구성하는 데 사용되며,
-        여러 필터링 조건을 동적으로 결합할 수 있음.
-         */
-        BooleanBuilder predicate = new BooleanBuilder();
-        if (!"all".equalsIgnoreCase(status)) {
-            predicate.and(qTripGroup.status.eq(GroupStatus.valueOf(status.toUpperCase())));
-        }
-        if (!"all".equalsIgnoreCase(area)) {
-            predicate.and(qTripGroup.tripArea.eq(area));
-        }
-        if (title != null && !title.trim().isEmpty()) {
-            predicate.and(qTripGroup.name.containsIgnoreCase(title.trim()));
-        }
 
-        List<TripGroup> tripGroups = queryFactory
-            .selectFrom(qTripGroup)
-            .where(predicate)
-            .leftJoin(qTripGroup.member).fetchJoin()
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .orderBy(sortOrder(pageable.getSort()).stream().toArray(OrderSpecifier[]::new))
-            .fetch();
-
+        List<TripGroup> tripGroups = tripGroupRepository.findFilteredTripGroups(status, area, title, pageable);
         List<TripGroupResponseDto> tripGroupResponseDtoList = tripGroups.stream()
             .map(TripGroupResponseDto::from)
             .collect(Collectors.toList());
 
-        long total = queryFactory
-            .select(qTripGroup.count())
-            .from(qTripGroup)
-            .where(predicate)
-            .fetchOne();
-
+        long total = tripGroupRepository.countFilteredTripGroups(status, area, title);
         int totalPages = (int) Math.ceil((double) total / pageable.getPageSize());
         boolean isLast = pageable.getOffset() + pageable.getPageSize() >= total;
 
@@ -83,17 +49,26 @@ public class TripGroupListService {
         );
     }
 
-    // 스프링의 Sort 객체를 QueryDSL OrderSpecifier로 변환
-    private List<OrderSpecifier> sortOrder(Sort sort) {
-        List<OrderSpecifier> orders = new ArrayList<>();
-        //sort
-        sort.stream().forEach(order -> {
-            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
-            String prop = order.getProperty();
-            PathBuilder orderByExpression = new PathBuilder<>(TripGroup.class, "tripGroup");
-            orders.add(new OrderSpecifier(direction, orderByExpression.get(prop)));
-        });
-        return orders;
+    public TripGroupDetailResponseDto getTripGroupDetail(Long tripGroupId) {
+        TripGroup tripGroup = tripGroupRepository.findByTripGroupId(tripGroupId)
+            .orElseThrow(() -> new EntityNotFoundException("등록된 여행 정보가 없습니다. id: " + tripGroupId));
+
+        TripGroupResponseDto tripGroupResponseDto = TripGroupResponseDto.from(tripGroup);
+
+        List<GroupMemberDetailResponseDto> groupMembers = groupMemberRepository
+            .findApprovedMembersByTripGroupId(tripGroup.getTripGroupId())
+            .stream()
+            .map(GroupMemberDetailResponseDto::from)
+            .collect(Collectors.toList());
+
+        List<PlaceDetailResponseDto> places = placeRepository.findByTripGroup(tripGroup)
+            .stream()
+            .map(PlaceDetailResponseDto::from)
+            .collect(Collectors.toList());
+
+        return TripGroupDetailResponseDto.from(
+            tripGroupResponseDto, groupMembers, places
+        );
     }
 
 }
