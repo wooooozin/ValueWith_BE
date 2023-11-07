6c727d942a33ddc41e2243c332b691550a23df6e
dodunge <dodunge@gmail.com>
2023-11-07 15:22:22 +0900

    feat: [014-ALERT] 그룹 삭제에서 알림 기능 구현 초기 단계

--- src/main/java/com/valuewith/tweaver/group/service/TripGroupService.java
+++ src/main/java/com/valuewith/tweaver/group/service/TripGroupService.java
@@ -1,5 +1,8 @@
 package com.valuewith.tweaver.group.service;
 
+import com.valuewith.tweaver.alert.dto.AlertRequestDto;
+import com.valuewith.tweaver.alert.service.AlertService;
+import com.valuewith.tweaver.constants.AlertContent;
 import com.valuewith.tweaver.constants.GroupStatus;
 import com.valuewith.tweaver.constants.ImageType;
 import com.valuewith.tweaver.defaultImage.entity.DefaultImage;
@@ -8,6 +11,7 @@ import com.valuewith.tweaver.defaultImage.service.ImageService;
 import com.valuewith.tweaver.group.dto.TripGroupRequestDto;
 import com.valuewith.tweaver.group.entity.TripGroup;
 import com.valuewith.tweaver.group.repository.TripGroupRepository;
+import com.valuewith.tweaver.groupMember.repository.GroupMemberRepository;
 import com.valuewith.tweaver.member.entity.Member;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Transactional
 public class TripGroupService {
   private final ImageService imageService;
+  private final AlertService alertService;
 
   private final TripGroupRepository tripGroupRepository;
   private final DefaultImageRepository defaultImageRepository;
@@ -79,5 +84,24 @@ public class TripGroupService {
 
   public void deleteTripGroup(Long tripGroupId) {
     tripGroupRepository.deleteById(tripGroupId);
+  }
+
+  public void sendTripGroupAlert(Long tripGroupId) {
+    Member member = Member.builder()
+        .memberId(1L)
+        .email("dodunge@gmail.com")
+        .password("1234")
+        .nickName("수정")
+        .age(20)
+        .gender("여성")
+        .profileUrl("http://images...")
+        .isSocial(true)
+        .build();
+    alertService.send(AlertRequestDto.builder()
+        .userToken("aaa")
+        .groupId(tripGroupId)
+        .member(member)
+        .content(AlertContent.DELETED_GROUP)
+        .build());
   }
 }
