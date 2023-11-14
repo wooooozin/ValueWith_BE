a9b1cb77b07bc2cf197b2ab35d4472e204f2893f
wooooozin <wooooozin@gmail.com>
2023-11-14 14:25:49 +0900

    feat: 그룹멤버 추방하기 컨트롤러 작성

--- src/main/java/com/valuewith/tweaver/groupMember/controller/GroupMemberListController.java
+++ src/main/java/com/valuewith/tweaver/groupMember/controller/GroupMemberListController.java
@@ -1,17 +1,20 @@
 package com.valuewith.tweaver.groupMember.controller;
 
 import com.valuewith.tweaver.commons.security.service.TokenService;
-import com.valuewith.tweaver.group.repository.TripGroupRepository;
-import com.valuewith.tweaver.groupMember.dto.GroupMemberDto;
 import com.valuewith.tweaver.groupMember.dto.GroupMemberListDto;
 import com.valuewith.tweaver.groupMember.service.GroupMemberListService;
 import com.valuewith.tweaver.member.entity.Member;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -43,5 +46,18 @@ public class GroupMemberListController {
         String memberEmail = tokenService.getMemberEmail(token);
         groupMemberListService.leftMemberFromTripGroup(memberEmail, tripGroupId);
         return ResponseEntity.ok("여행그룹에서 나갔습니다.");
+    }
+
+    @PatchMapping("{tripGroupId}/member/{groupMemberId}/banned")
+    public ResponseEntity<String> bannedGroupMemberFromTripGroup(
+        @RequestHeader("Authorization") String token,
+        @PathVariable Long tripGroupId,
+        @PathVariable Long groupMemberId
+    ) {
+        String memberEmail = tokenService.getMemberEmail(token);
+        Member member = groupMemberListService.bannedGroupMemberFromTripGroup(
+            memberEmail, tripGroupId, groupMemberId
+        );
+        return ResponseEntity.ok( member.getNickName() + " 님을 추방 했습니다.");
     }
 }
