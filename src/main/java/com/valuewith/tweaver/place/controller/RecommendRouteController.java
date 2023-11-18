48b7d28e00159a8a6b090122da4865ee716b1961
dodunge <dodunge@gmail.com>
2023-11-18 15:47:59 +0900

    feat: [026-RECOMMEND/ROUTE] 추천경로 로직 구현

---
+++ src/main/java/com/valuewith/tweaver/place/controller/RecommendRouteController.java
@@ -0,0 +1,26 @@
+package com.valuewith.tweaver.place.controller;
+
+import com.valuewith.tweaver.place.dto.RecommendRouteDto;
+import com.valuewith.tweaver.place.service.RecommendRouteService;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recommend/route/*")
+public class RecommendRouteController {
+  private final RecommendRouteService recommendRouteService;
+
+  @GetMapping
+  public ResponseEntity<List<RecommendRouteDto>> recommendRoute(@RequestBody List<RecommendRouteDto> places) {
+    return ResponseEntity.ok(recommendRouteService.recommendRoute(places));
+  }
+}
