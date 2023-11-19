493348d26fed208409fd24d509a6ef9ed10203e9
wooooozin <95316662+wooooozin@users.noreply.github.com>
2023-11-19 23:35:04 +0900

    fix: url 수정

--- src/main/java/com/valuewith/tweaver/config/WebConfig.java
+++ src/main/java/com/valuewith/tweaver/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("https://tweaver.site")
+            .allowedOriginPatterns("https://tweaver.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
