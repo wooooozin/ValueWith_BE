d4160b0c0d02b0d133a5b77b0414d9b0fa21cc73
wooooozin <95316662+wooooozin@users.noreply.github.com>
2023-11-19 22:47:25 +0900

    feat: CORS(Cross-Origin Resource Sharing) 문제 따른 글로벌 CORS 구성

---
+++ src/main/java/com/valuewith/tweaver/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.valuewith.tweaver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+          .allowedOriginPatterns("hppts://tweaver.site")
+          .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "PATCH", "OPTIONS")
+          .allowedHeaders("")
+          .allowCredentials(true);
+    }
+}
