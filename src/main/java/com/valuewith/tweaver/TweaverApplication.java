57b5b538b2d30ad6254bea87e63119b31eb160c2
eod940 <eod940@gmail.com>
2023-11-22 16:53:44 +0900

    fix: [FIX-OAUTH2] Security 허용 URL 변경

--- src/main/java/com/valuewith/tweaver/TweaverApplication.java
+++ src/main/java/com/valuewith/tweaver/TweaverApplication.java
@@ -1,7 +1,9 @@
 package com.valuewith.tweaver;
 
+import com.valuewith.tweaver.config.AppPropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableFeignClients
 @EnableScheduling
+@EnableConfigurationProperties({AppPropertiesConfig.class})
 public class TweaverApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/valuewith/tweaver/config/AppPropertiesConfig.java
+++ src/main/java/com/valuewith/tweaver/config/AppPropertiesConfig.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
-@Configuration
 @ConfigurationProperties(prefix = "app")
 public class AppPropertiesConfig {
 
